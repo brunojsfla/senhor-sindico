8087c9c11dbb2c7e9a0aa7f1a371cdf0645cb7f1
unknown <brunojsfla@gmail.com>
2021-11-13 18:22:21 -0300

    chore: implementação da tela de Preload

---
+++ src/screens/LoginScreen/index.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import C from './styles';
+
+import {useStateValue} from '../../contexts/StateContext';
+import api from '../../services/api';
+
+export default () => {
+  const navigation = useNavigation();
+  const [context, dispatch] = useStateValue();
+
+  return (
+    <C.Container>
+      <C.Texto>Tela de Login</C.Texto>
+    </C.Container>
+  );
+};
