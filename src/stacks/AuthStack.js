62d730df7ab1b2cddb7e4f657718fe0b3a4b72a4
unknown <brunojsfla@gmail.com>
2021-11-14 07:37:47 -0300

    chore: implementação da tela de login

--- src/stacks/AuthStack.js
+++ src/stacks/AuthStack.js
@@ -1,23 +1,36 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 import PreloadScreen from '../screens/PreloadScreen';
-import LoginScreen from "../screens/LoginScreen";
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
 export default () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen 
-                name="PreloadScreen"
-                component={PreloadScreen}
-                options={{headerShown: false}}
-            />
-            <Stack.Screen 
-                name="LoginScreen"
-                component={LoginScreen}
-                options={{headerShown: false}}
-            />
-        </Stack.Navigator>
-    );
-}
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f5f6fa',
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+      }}>
+      <Stack.Screen
+        name="PreloadScreen"
+        component={PreloadScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{headerShown: true}}
+      />
+    </Stack.Navigator>
+  );
+};
